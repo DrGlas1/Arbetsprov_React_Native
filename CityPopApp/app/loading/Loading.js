0f9ee866dc7a43095ec7f6736e82a6b3913c2782
Elliot Glas <elliot.glas@hotmail.se>
2022-03-23 05:04:51 +0100

    Added documentation to Loading component

--- CityPopApp/app/loading/Loading.js
+++ CityPopApp/app/loading/Loading.js
@@ -2,6 +2,11 @@ import React from "react";
 import { ActivityIndicator, StyleSheet, View } from "react-native";
 import colors from "../config/colors";
 
+/**
+ * Shows a loading symbol when something hasn't loaded yet, like an image.
+ * @param {Boolean} loading Shows the loading symbol only when loading is true
+ * @returns An ActivityIndicator with an absolut position on the screeb
+ */
 function Loading({ loading }) {
   return (
     <View style={styles.loading}>
